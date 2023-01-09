73b5264ce0763e50d6b50690647d2832d1bb6fcd
mellabanca <91677889+mellabanca@users.noreply.github.com>
2023-01-09 14:57:55 -0300

    Aula 82 StoryCard e Feed

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,16 +1,104 @@
 import React, {Component} from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView, Platform,
+         StatusBar, Image, FlatList } from "react-native";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import StoryCard from "./StoryCard";
+import { RFValue } from "react-native-responsive-fontsize";
+SplashScreen.preventAutoHideAsync();
+
+let customFonts = {
+    "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+}
+
+let stories = require("./temp.json");
 
 export default class Feed extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            fontsLoaded: false
+        }
+    }
+
+    async _loadFontsAsync(){
+        await Font.loadAsync(customFonts);
+        this.setState({fontsLoaded: true})
+    }
+
+    componentDidMount(){
+        this._loadFontsAsync();
+    }
+
+    renderItem = ({ item: story }) => {
+        return <StoryCard story={story} />;
+      };
+    
+    keyExtractor = (item, index) => index.toString();
+
     render(){
+        if(!this.state.fontsLoaded){
+            SplashScreen.hideAsync();
+        } else {
         return(
-            <View style = {{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
-                <Text>Feed</Text>
+            <View style={styles.container}>
+            <SafeAreaView style={styles.droidSafeArea} />
+            <View style={styles.appTitle}>
+              <View style={styles.appIcon}>
+                <Image
+                  source={require("../assets/logo.png")}
+                  style={styles.iconImage}
+                ></Image>
+              </View>
+              <View style={styles.appTitleTextContainer}>
+                <Text style={styles.appTitleText}>App Narração de Histórias</Text>
+              </View>
             </View>
+            <View style={styles.cardContainer}>
+              <FlatList
+                keyExtractor={this.keyExtractor}
+                data={stories}
+                renderItem={this.renderItem}
+              />
+            </View>
+          </View>
         )
     }
 }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#15193c"
+    },
+    droidSafeArea: {
+      marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+      flex: 0.07,
+      flexDirection: "row"
+    },
+    appIcon: {
+      flex: 0.3,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    iconImage: {
+      width: "100%",
+      height: "100%",
+      resizeMode: "contain"
+    },
+    appTitleTextContainer: {
+      flex: 0.7,
+      justifyContent: "center"
+    },
+    appTitleText: {
+      color: "white",
+      fontSize: RFValue(28),
+      fontFamily: "Bubblegum-Sans"
+    },
+    cardContainer: {
+      flex: 0.93
+    }
+  });
